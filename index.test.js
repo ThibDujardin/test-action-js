8052eb386e6a651fcdd4f81bdbd7f507f3d3f29e
Thibauld Dujardin <thibauld.dujardin.partner@decathlon.com>
2022-09-21 21:16:55 +0200

    wip

--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
-const wait = require('./wait');
+const { add, subtract, multiply } = require("./math");
+
 const process = require('process');
 const cp = require('child_process');
 const path = require('path');
@@ -6,25 +7,13 @@ const path = require('path');
 test('throws invalid number', async () => {
   await expect(wait('foo')).rejects.toThrow('milliseconds not a number');
 });
-
-test('wait 500 ms', async () => {
-  const start = new Date();
-  await wait(500);
-  const end = new Date();
-  var delta = Math.abs(end - start);
-  expect(delta).toBeGreaterThanOrEqual(500);
-});
-
-// shows how the runner will run a javascript action with env / stdout protocol
-test('test runs', () => {
-  process.env['INPUT_MILLISECONDS'] = 100;
-  const ip = path.join(__dirname, 'index.js');
-  const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
-  console.log(result);
-})
-
-const { add, subtract, multiply } = require("../math");
-
+// // shows how the runner will run a javascript action with env / stdout protocol
+// test('test runs', () => {
+//   process.env['INPUT_MILLISECONDS'] = 100;
+//   const ip = path.join(__dirname, 'index.js');
+//   const result = cp.execSync(`node ${ip}`, {env: process.env}).toString();
+//   console.log(result);
+// })
 describe("simple arithmetic", () => {
   describe("addition", () => {
     test("expect 2 + 3 = 5", () => {
