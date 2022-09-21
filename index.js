b65bee659dcbcc2f0ee927929bd9d9b5f0dec15d
Thibauld Dujardin <thibauld.dujardin.partner@decathlon.com>
2022-09-21 21:00:25 +0200

    WIP

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const core = require('@actions/core');
-const wait = require('./wait');
-
-
-// most @actions toolkit packages have async methods
+const add = require('./add')
+const subtract = require('./subtract')
+const multiply = require('./multiply')
+ 
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const input1 = core.getInput('input-1');
+    const input2 = core.getInput('input-2');
+    const start = new Date();
+    core.debug('Starting at : ' + start.toTimeString());
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    core.info(`Performin addition of : ${input1} & ${input2} ...`);
+    const addResult = add(input1,input2)
 
-    core.setOutput('time', new Date().toTimeString());
+    core.info(`Performin subtraction of : ${input1} & ${input2} ...`);
+    const subtractResult = subtract(input1,input2)
+
+    core.info(`Performin multiplication of : ${input1} & ${input2} ...`);
+    const multiplyResult = multiply(input1,input2)
+    const end = new Date();
+    core.debug('Ending at : ' + end.toTimeString());
+
+    core.warning('It take : ' + (end - start));
+ 
+
+    core.setOutput('addition', addResult);
+    core.setOutput('subtract', subtractResult);
+    core.setOutput('multiplication', multiplyResult);
   } catch (error) {
     core.setFailed(error.message);
   }
